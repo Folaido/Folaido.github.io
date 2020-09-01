7130922b4f81b52a480a4d886380191cd4113b5d
Folaido <feizr424@gmail.com>
2020-09-01 16:09:03 -0500

    Updates

---
+++ precache-manifest.9f392e712b714ed67264682e96b4b970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45cc36c7db3b939b3b54df6f2c93619",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c300de2eacd0863eda7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a275865bf26c8d870b9",
+    "url": "/static/js/2.e465fead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e465fead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c300de2eacd0863eda7",
+    "url": "/static/js/main.c75610a4.chunk.js"
+  },
+  {
+    "revision": "2d5fad56ef2d45370834",
+    "url": "/static/js/runtime-main.ab3c82de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
